9e2bffaeff8381c4fd1c63ebd29604428c37b0b4
Vasiliy Vinogradov <vase3k@gmail.com>
2025-05-27 19:51:37 +0300

    задача со звёздочкой

--- src/js/script.js
+++ src/js/script.js
@@ -59,3 +59,16 @@
 //     return arrayOfNumbers;
 // }
 // fifthTask();
+
+// const lines = 5;
+// let result = '';
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 1; j <= lines - i; j++) {
+//         result += ' ';
+//     }
+//     for (let k = 0; k <= i * 2; k++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+// console.log(result);
