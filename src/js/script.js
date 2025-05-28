439644b1bdf6aaa98fb84a31e90415993142e649
Vasiliy Vinogradov <vase3k@gmail.com>
2025-05-28 22:47:59 +0300

    strings

--- src/js/script.js
+++ src/js/script.js
@@ -1,78 +1,8 @@
 'use strict';
 
-import '/src/js/personalMovieDB.js';
-import '/src/js/functions.js';
 import '/src/sass/style.scss';
-
-// const drawPiramide = (length, acc = '') => {
-//     for (let i = 1; i <= length; i++) {
-//         for (let j = 0; j < i; j++) {
-//             acc += '*';
-//         }
-//         acc += '\n';
-//     }
-//     return acc;
-// };
-
-// first: for (let i = 0; i <= 3; i++) {
-//     console.log(`first level:${i}`);
-//     for (let j = 0; j <= 3; j++) {
-//         console.log(`second level:${j}`);
-//         for (let k = 0; k <= 3; k++) {
-//             if (k === 2) break first;
-//             console.log(`third level:${k}`);
-//         }
-//     }
-// }
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-// let i = 2;
-// while (i <= 16) {
-//     if (i % 2 === 0) {
-//         i++;
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-//     i++;
-// }
-
-// function fifthTask() {
-//     const arrayOfNumbers = [];
-
-//     // Пишем решение вот тут
-//     let i = 5;
-//     while (i <= 10) {
-//         if (i < 5) {
-//             i++;
-//             continue;
-//         }
-//         arrayOfNumbers[i - 5] = i;
-//         i++;
-//     }
-
-//     // Не трогаем
-//     console.log(arrayOfNumbers);
-//     return arrayOfNumbers;
-// }
-// fifthTask();
-
-// const lines = 5;
-// let result = '';
-// for (let i = 0; i <= lines; i++) {
-//     for (let j = 1; j <= lines - i; j++) {
-//         result += ' ';
-//     }
-//     for (let k = 0; k <= i * 2; k++) {
-//         result += '*';
-//     }
-//     result += '\n';
-// }
-// console.log(result);
+//import '/src/js/personalMovieDB.js';
+//import '/src/js/functions.js';
+//import '/src/js/cycles.js';
+//import '/src/js/string&numbers.js';
+import '/src/js/functionsAdvance.js';
---
+++ src/js/string&numbers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const str = 'something got wrong';
+const arr = [1, 2, 3, 4, 5];
+const num = 12.2;
+
+const test = '12.2px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
