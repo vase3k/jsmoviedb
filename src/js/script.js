850ee1e7bd93a8d15e721ab812c5d88d628e1ee7
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-03 19:50:15 +0300

    getElements

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import '/src/sass/style.scss';
-import '/src/js/personalMovieDB.js';
+//import '/src/js/personalMovieDB.js';
 //import '/src/js/functions.js';
 //import '/src/js/cycles.js';
 //import '/src/js/string&numbers.js';
@@ -11,3 +11,4 @@ import '/src/js/personalMovieDB.js';
 //import '/src/js/OPP.js';
 //import '/src/js/circut.js';
 //import '/src/js/debug.js';
+import '/src/js/elements.js';
